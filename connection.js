6a6535375258d7e3a36bb9f377b22508842a573d
catherineMena <clairejaen99@gmail.com>
2025-03-26 20:00:00 -0600

    fix: load env vars from Railway

--- connection.js
+++ connection.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+
+const mysql = require('mysql2/promise');
+
+
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -7,3 +12,5 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
+
+module.exports = () => pool;
